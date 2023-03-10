2968d253d0cf3d6183f4de9381fd5a1f5f2423c0
Jayphen <software@jayphen.com>
2023-03-10 20:18:08 +0100

    Add some mappings

--- mappings.lua
+++ mappings.lua
@@ -15,8 +15,11 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
+    [";"] = { ":" },
+    [";;"] = { ";" },
+    ["<leader>bf"] = { ":Neotree float buffers<cr>" },
+    ["<leader>gs"] = { ":Neotree float git_status<cr>" }
   },
   t = {
     -- setting a mapping to false will disable it
