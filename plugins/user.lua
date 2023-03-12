3c6f46a6997286d8bba2186120288488707e0f0d
Jayphen <software@jayphen.com>
2023-03-12 21:47:16 +0100

    Add plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,20 @@
 return {
+  {
+    'ruifm/gitlinker.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = function()
+      require("gitlinker").setup()
+    end
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
