be59dcc293133db860bdd63c05d3177bc9ab50cd
Jayphen <jayphen.simpson@n26.com>
2023-04-21 13:02:00 +0200

    Switch to catppuccin

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,6 @@ return {
 
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.colorscheme.catppuccin" }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
